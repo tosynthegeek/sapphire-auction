a39ea51a33494963c73167f0bd45fa34f3bd93d6
tosynthegeek <121390044+tosynthegeek@users.noreply.github.com>
2024-05-06 17:04:07 +0100

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,9 +9,15 @@ const config: HardhatUserConfig = {
     "sapphire-testnet": {
       url: "https://testnet.sapphire.oasis.io",
       accounts: [
-        process.env.PRIVATE_KEY,
+        "7cf14cea97ee9ace52e647f282686ef9bd32c5fd686272190520289905885293",
       ],
       chainId: 0x5aff,
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_API_KEY_URL,
+      accounts: [
+        "7cf14cea97ee9ace52e647f282686ef9bd32c5fd686272190520289905885293",
+      ],
     },
   },
 };
